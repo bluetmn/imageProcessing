ab3cd2a2ff5aff618c6e4e2ec779f2428beb11a5
Thomas Ryan <thomasryan2118@gmail.com>
2023-07-27 18:56:25 -0500

    produces a readable graph

--- main.py
+++ main.py
@@ -78,12 +78,33 @@ def get_AAI(image):
 
 time_series = filtered_aq.map(get_AAI)
 
-# dates = time_series.aggregate_array('system:time_start').getInfo()
-# values = time_series.aggregate_array('absorbing_aerosol_index').getInfo()
-
 AAI_values_list = time_series.getRegion(roi, scale=30).getInfo()
+
 dates = [value[0] for value in AAI_values_list[1:]]
 values = [value[-1] for value in AAI_values_list[1:]]
+
+c = 0
+while c < len(values):
+    if values[c] == None:
+        values.pop(c)
+        dates.pop(c)
+    else:
+        c += 1
+
+reducedDates = []
+for c in range(len(dates)):
+    if dates[c][4] == 0:
+        newDate = dates[c][5:6] + '-' + dates[c][6:8]
+    else:
+        newDate = dates[c][4:6] + '-' + dates[c][6:8]
+    dates[c] = newDate
+    if c % 10 == 0:
+        reducedDates.append(newDate)
+
+print(dates)
+print('\n\n\n')
+print(values)
+
 
 # Plot the time series
 plt.figure(figsize=(10, 6))
@@ -92,6 +113,7 @@ plt.xlabel('Date')
 plt.ylabel('AAI')
 plt.title(f'AAI Time Series')
 plt.xticks(rotation=45)
+plt.xticks(reducedDates)
 plt.grid(True)
 plt.tight_layout()
 plt.show()
